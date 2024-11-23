19ab13b3e7a4373b3a4771cfca58cbd6b3ecfabc
Neha kumari <kumarineha122162@gmail.com>
2024-11-23 20:29:04 +0530

    update features

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,5 +1,6 @@
 const UserModel = require("../model/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 
 const Signup = async(req,res) => {
     try{
@@ -17,4 +18,34 @@ const Signup = async(req,res) => {
     }
 }
 
-module.exports={Signup};
+const Login = async(req,res) => {
+    try{
+        const { email, password } = req.body;
+        const user = await UserModel.findOne({email});
+        const errorMsg = 'Auth failed email or passwprd is wrong';
+        if(!user){
+            return res.status(403).json({message:errorMsg, success:false});
+        }
+        const isPassEqual = await bcrypt.compare(password, user.password);
+        if(!isPassEqual){
+            return res.status(403).json({message: errorMsg, success: false});
+        }
+        const jwtToken = jwt.sign(
+            { email: user.email, _id: user._id },
+            process.env.JWT_SECRET,
+            { expireIn: '24h'}
+        )
+
+        res.status(200).json({
+            message: "Login Success",
+            success: true,
+            jwtToken,
+            email,
+            name: user.name
+        })
+    } catch(err){
+        res.status(500).json({message: "Internal Server error", success:false});
+    }
+}
+
+module.exports={ Signup, Login };
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 require('./model/db');
-const {SignupValidation} = require("./middlewares/AuthValidation");
-const { Signup } = require("./controllers/AuthController");
+const {SignupValidation, LoginValidation} = require("./middlewares/AuthValidation");
+const { Signup, Login } = require("./controllers/AuthController");
 
 const express = require('express');
 const app = express();
@@ -13,9 +13,7 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/login', (req,res)=>{
-    res.send("login success");
-})
+app.post('/login', LoginValidation, Login);
 app.post("/signup", SignupValidation, Signup);
 
 app.listen(PORT, ()=>{
